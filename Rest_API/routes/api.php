3bd72e961067ebb91535e12f80d1f09a03693f56
MD. Nazmul Hasan Anik <anikology@gmail.com>
2022-07-25 22:30:09 +0600

    Crud Auth

--- Rest_API/routes/api.php
+++ Rest_API/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DummyAPIController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\CountryController;
+use App\Http\Controllers\API\AuthController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,3 +26,18 @@ Route::get("device/{id?}",[DeviceController::class,'list']);
 Route::get("country",[CountryController::class,'all']);
 Route::get("country/{id}",[CountryController::class,'countrybyid']);
 Route::post("country",[CountryController::class,'savecountry']);
+//API route for register new user
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('programs', App\Http\Controllers\API\ProgramController::class);
+
+    // API route for logout user
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);});
